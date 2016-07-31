0c8e19af6376670e48a9c86463ec24af636d500f
Eric <sferic@163.com>
2016-07-31 15:46:32 +0800

    初始化项目

---
+++ NinesquareAndXib/NinesquareAndXib/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  NinesquareAndXib
+//
+//  Created by ma c on 16/7/31.
+//  Copyright © 2016年 shifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ NinesquareAndXib/NinesquareAndXib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NinesquareAndXib
+//
+//  Created by ma c on 16/7/31.
+//  Copyright © 2016年 shifei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
